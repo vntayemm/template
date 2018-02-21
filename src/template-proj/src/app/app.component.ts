b67fbd5fbc4bf2a4476767b52d5cebdf14919740
minh.nguyen@tpssoft.com <minh.nguyen@tpssoft.com>
2018-02-21 12:51:18 +0700

    toggle left menu

--- src/template-proj/src/app/app.component.ts
+++ src/template-proj/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+
+  isHidden: boolean = false;
+
+  menuToggle() {
+    this.isHidden = !this.isHidden;
+  }
 }
